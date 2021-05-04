4f429f6dc87b14fb51eee60bcc7c309dadc43106
Amaniitd <amanamanwrs121@gmail.com>
2021-05-04 11:37:07 +0530

    rigidity in walls

--- main.cpp
+++ main.cpp
@@ -1,17 +1,96 @@
 #include <iostream>
 #include <SDL2/SDL.h>
 #include <stdio.h>
+#include <fstream>
+#include <string>
+
+using namespace std;
 
 const int SCREEN_WIDTH = 960;
 const int SCREEN_HEIGHT = 640;
 
+void printTile(int *tiles)
+{
+    for (int i = 0; i < 20; i++)
+    {
+        for (int j = 0; j < 30; j++)
+        {
+            cout << tiles[30 * i + j] << " ";
+        }
+        cout << endl;
+    }
+}
+
+SDL_Surface *createMapSurface(int *tiles)
+{
+    SDL_Surface *surf = SDL_CreateRGBSurface(0, SCREEN_WIDTH, SCREEN_HEIGHT, 32, 0, 0, 0, 0);
+    int row = 0;
+    int column = 0;
+    SDL_Rect rectWall;
+    rectWall.w = 32;
+    rectWall.h = 32;
+
+    while (row < SCREEN_HEIGHT / 32)
+    {
+        while (column < SCREEN_WIDTH / 32)
+        {
+            rectWall.x = column * 32;
+            rectWall.y = row * 32;
+            int currentTile = tiles[row * 30 + column];
+            Uint32 color;
+            if (currentTile == 0)
+            {
+                color = SDL_MapRGB(surf->format, 0, 0, 0);
+            }
+            else if (currentTile == 1)
+            {
+                color = SDL_MapRGB(surf->format, 255, 255, 255);
+            }
+            SDL_FillRect(surf, &rectWall, color);
+            column++;
+        }
+        column = 0;
+        row++;
+    }
+    if (surf == NULL)
+    {
+        SDL_Log("SDL_CreateRGBSurface() failed: %s", SDL_GetError());
+        exit(1);
+    }
+    return surf;
+}
+
+void loadTiles(int *tiles)
+{
+    ifstream tileFile("tiles.txt");
+    if (tileFile.is_open())
+    {
+        int tile;
+        int i = 0;
+        while (tileFile >> tile)
+        {
+            tiles[i] = tile;
+            i++;
+        }
+        tileFile.close();
+    }
+}
+
+bool checkWall(int *lists, int x, int y)
+{
+    return lists[(y / 32) * (SCREEN_WIDTH / 32) + x / 32];
+}
+
 int main(int argc, char *args[])
 {
+
     //The window we'll be rendering to
     SDL_Window *window = NULL;
 
     //The surface contained by the window
     SDL_Surface *screenSurface = NULL;
+
+    SDL_Surface *walls;
 
     //Initialize SDL
     if (SDL_Init(SDL_INIT_VIDEO) < 0)
@@ -31,6 +110,11 @@ int main(int argc, char *args[])
             //Get window surface
             screenSurface = SDL_GetWindowSurface(window);
 
+            int tiles[600];
+            loadTiles(tiles);
+            // printTile(tiles);
+
+            walls = createMapSurface(tiles);
             //Fill the surface white
 
             SDL_Event event;
@@ -52,37 +136,60 @@ int main(int argc, char *args[])
                     {
                         if (event.key.keysym.sym == SDLK_RIGHT)
                         {
-                            rect.x = rect.x + 32;
+                            rect.x += 32;
                             rect.x = rect.x % SCREEN_WIDTH;
+                            if (checkWall(tiles, rect.x, rect.y))
+                            {
+                                rect.x -= 32;
+                                rect.x += SCREEN_WIDTH;
+                                rect.x = rect.x % SCREEN_WIDTH;
+                            }
                         }
                         else if (event.key.keysym.sym == SDLK_LEFT)
                         {
-                            rect.x = rect.x - 32;
-                            rect.x = (SCREEN_WIDTH + rect.x) % SCREEN_WIDTH;
+                            rect.x -= 32;
+                            rect.x += SCREEN_WIDTH;
+                            rect.x = rect.x % SCREEN_WIDTH;
+                            if (checkWall(tiles, rect.x, rect.y))
+                            {
+                                rect.x += 32;
+                                rect.x = rect.x % SCREEN_WIDTH;
+                            }
                         }
                         else if (event.key.keysym.sym == SDLK_DOWN)
                         {
-                            rect.y = rect.y + 32;
+                            rect.y += 32;
                             rect.y = rect.y % SCREEN_HEIGHT;
+                            if (checkWall(tiles, rect.x, rect.y))
+                            {
+                                rect.y -= 32;
+                                rect.y += SCREEN_HEIGHT;
+                                rect.y = rect.y % SCREEN_HEIGHT;
+                            }
                         }
                         else if (event.key.keysym.sym == SDLK_UP)
                         {
-                            rect.y = rect.y - 32;
-                            rect.y = (SCREEN_HEIGHT + rect.y) % SCREEN_HEIGHT;
+                            rect.y -= 32;
+                            rect.y += SCREEN_HEIGHT;
+                            rect.y = rect.y % SCREEN_HEIGHT;
+                            if (checkWall(tiles, rect.x, rect.y))
+                            {
+                                rect.y += 32;
+                                rect.y = rect.y % SCREEN_HEIGHT;
+                            }
                         }
                     }
                 }
-                SDL_FillRect(screenSurface, NULL, SDL_MapRGB(screenSurface->format, 3, 252, 127));
+                // SDL_FillRect(screenSurface, NULL, SDL_MapRGB(screenSurface->format, 3, 252, 127));
+                SDL_BlitSurface(walls, NULL, screenSurface, NULL);
                 SDL_FillRect(screenSurface, &rect, SDL_MapRGB(screenSurface->format, 161, 66, 245));
                 SDL_UpdateWindowSurface(window);
             }
 
-            //Update the surface
-            SDL_UpdateWindowSurface(window);
-
             //Wait two seconds
         }
     }
+    SDL_FreeSurface(walls);
     //Destroy window
     SDL_DestroyWindow(window);
 
